6e77f4fa8b4305793ad1d311e4ef7c1d5d80aab1
curlex <curlex@hotmail.com>
2015-10-12 16:45:44 +0100

    Test2

--- AndroidBlackTiger/app/src/main/java/abt/androidblacktiger/MainActivity.java
+++ AndroidBlackTiger/app/src/main/java/abt/androidblacktiger/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
+//hi
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will

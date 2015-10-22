b1c8e1f48fbb19c78feb06ede7b3770e62277fe5
Maria47127 <maria.treacy@ucdconnect.ie>
2015-10-22 09:26:16 +0100

    The schema for the DB

---
+++ AndroidBlackTiger/app/src/main/java/abt/androidblacktiger/HistoryDBHandeler.java
@@ -0,0 +1,95 @@
+package abt.androidblacktiger;
+
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Maria on 22/10/2015.
+ */
+public class HistoryDBHandeler  extends SQLiteOpenHelper {
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+    // Database Name
+    private static final String DATABASE_NAME = "HistoryDB";
+    // Database Table
+    private static final String TABLE_HISTORY = "History";
+
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_WORD = "word";
+    public static final String COLUMN_TRANSLATION = "translation";
+    public static final String COLUMN_LOCATION = "location";
+    public static final String COLUMN_SHOWN = "shown";
+    public static final String COLUMN_IMAGE = "image";
+
+    public HistoryDBHandeler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_TABLE_HISTORY = "CREATE TABLE " +
+                TABLE_HISTORY + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY,"
+                + COLUMN_WORD + " TEXT,"
+                + COLUMN_TRANSLATION + " TEXT,"
+                + COLUMN_LOCATION + " TEXT,"
+                + COLUMN_SHOWN + " INTEGER,"
+                + COLUMN_IMAGE + " TEXT"
+                + ")";
+        db.execSQL(CREATE_TABLE_HISTORY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion,
+                          int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_HISTORY);
+        onCreate(db);
+    }
+
+    public void addWordHistory(WordHistory wordData) {
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_WORD, wordData.getWord());
+        values.put(COLUMN_TRANSLATION, wordData.getTranslation());
+        values.put(COLUMN_LOCATION, wordData.getTranslation());
+        values.put(COLUMN_SHOWN, wordData.getShown());
+        values.put(COLUMN_IMAGE, wordData.getImagePath());
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.insert(TABLE_HISTORY, null, values);
+        db.close();
+    }
+
+    public WordHistory findWord(String word) {
+        String query = "Select * FROM " + TABLE_HISTORY + " WHERE " + COLUMN_WORD + " =  \"" + word + "\"";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Cursor cursor = db.rawQuery(query, null);
+
+        WordHistory data = new WordHistory();
+
+        if (cursor.moveToFirst()) {
+            cursor.moveToFirst();
+            data.setID(Integer.parseInt(cursor.getString(0)));
+            data.setWord(cursor.getString(1));
+            data.setTranslation(cursor.getString(2));
+            data.setLocation(cursor.getString(3));
+            data.setShown(Integer.parseInt(cursor.getString(4)));
+            data.setLocation(cursor.getString(5));
+            cursor.close();
+        } else {
+            data = null;
+        }
+        db.close();
+        return data;
+    }
+}
+
+

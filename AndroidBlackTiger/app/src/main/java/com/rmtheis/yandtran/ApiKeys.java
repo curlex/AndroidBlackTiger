c2dc93eaefd81fa3998f915588839dd5d175d9d7
Diarmuid <diarmuidr3d@gmail.com>
2015-10-23 12:51:45 +0100

    Added Yandex Translate API

---
+++ AndroidBlackTiger/app/src/main/java/com/rmtheis/yandtran/ApiKeys.java
@@ -0,0 +1,6 @@
+package com.rmtheis.yandtran;
+
+public class ApiKeys {
+//  protected static final String YANDEX_API_KEY = "[Put your API key here]";
+  public static final String YANDEX_API_KEY = "trnsl.1.1.20151023T103921Z.a96fdab4bb0af2db.2516370694c133e16642d4b72a29cf68904819be";
+}

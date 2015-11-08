2aa86edb3ad1495a036963dfdd54e956a8aa37d4
Olivia <olivia_ranito@hotmail.com>
2015-11-08 14:15:48 +0000

    Updated

--- AndroidBlackTiger/app/src/main/java/abt/androidblacktiger/Logo_fragment.java
+++ AndroidBlackTiger/app/src/main/java/abt/androidblacktiger/Logo_fragment.java
@@ -1,25 +1,21 @@
 package abt.androidblacktiger;
 
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import abt.androidblacktiger.HistoryDBHandeler;
 
-public class EnglishWordFragment extends Fragment{
+/**
+ * Created by Olivia on 08/11/2015.
+ */
+public class Logo_fragment extends Fragment{
 
-    //read the word from the database
-
-
-
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.eng_word_fragment, container, false);
+        View view = inflater.inflate(R.layout.logo_fragment, container, false);
         return view;
     }
-
 }

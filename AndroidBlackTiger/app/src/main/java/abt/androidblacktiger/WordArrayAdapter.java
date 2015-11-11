c4efbb286e655e1897805e188b21ce4481149264
Diarmuid <diarmuidr3d@gmail.com>
2015-11-11 11:01:35 +0000

    Fixed warnings in WordArrayAdapter

--- AndroidBlackTiger/app/src/main/java/abt/androidblacktiger/WordArrayAdapter.java
+++ AndroidBlackTiger/app/src/main/java/abt/androidblacktiger/WordArrayAdapter.java
@@ -1,7 +1,6 @@
 package abt.androidblacktiger;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,18 +9,13 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-import static android.support.v4.content.ContextCompat.getColor;
-
 /**
- * Created by Diarmuid on 10/11/2015.
+ * Created by Diarmuid Ryan. For use with ListFragment.
  */
 public class WordArrayAdapter extends ArrayAdapter<WordHistory> {
 
     private final Context context;
     private final ArrayList<WordHistory> words;
-    private final int resource = R.layout.textviewlay;
-    private final int wordView = R.id.history_textview;
-    private final int transView = R.id.translation_textview;
 
     public WordArrayAdapter(Context context, ArrayList<WordHistory> words) {
         super(context, R.layout.textviewlay);
@@ -33,13 +27,16 @@ public class WordArrayAdapter extends ArrayAdapter<WordHistory> {
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         if(view == null) {
+            int resource = R.layout.textviewlay;
             view = inflater.inflate(resource, parent, false);
         }
+        int wordView = R.id.history_textview;
         TextView textView = (TextView) view.findViewById(wordView);
         WordHistory wordHistory = words.get(position);
         String word = wordHistory.getWord();
         textView.setText(word);
         String translation = wordHistory.getTranslation();
+        int transView = R.id.translation_textview;
         TextView translatedView = (TextView) view.findViewById(transView);
         translatedView.setText(translation);
         return view;

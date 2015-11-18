477188e570a562d1f301abefe4ac367f91fb01cd
Maria47127 <maria.treacy@ucdconnect.ie>
2015-11-18 14:22:42 +0000

    images fixed

--- AndroidBlackTiger/app/src/main/java/abt/androidblacktiger/MainMenuActivity.java
+++ AndroidBlackTiger/app/src/main/java/abt/androidblacktiger/MainMenuActivity.java
@@ -5,21 +5,12 @@ package abt.androidblacktiger;
  */
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -27,9 +18,9 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
 
-        Button button_word_history = (Button) findViewById(R.id.word_history_button);
-        Button button_dictionary = (Button) findViewById(R.id.dictionary_button);
-        Button button_Gps = (Button) findViewById(R.id.gps_button);
+        ImageButton button_word_history = (ImageButton) findViewById(R.id.word_history_button);
+        // Button button_dictionary = (Button) findViewById(R.id.dictionary_button);
+        //Button button_Gps = (Button) findViewById(R.id.gps_button);
         ImageButton button_map_discovery = (ImageButton) findViewById(R.id.map_discovery_button);
         button_map_discovery.setBackgroundResource(R.drawable.mao); // name map gave error :P
         startService(new Intent(this, GPS.class));
@@ -65,14 +56,14 @@ public class MainMenuActivity extends AppCompatActivity {
         {
             intent = new Intent(this, HistoryActivity.class);
         }
-        else if (view.getId()==R.id.dictionary_button)
+        /*else if (view.getId()==R.id.dictionary_button)
         {
             //Replace MainMenuActivity.class with the name of activity corresponding to the dictionary
 
         }
         else if(view.getId()==R.id.gps_button){
             intent = new Intent(this, NewGPS.class);
-        }
+        }*/
         else
         {
             //Replace MainMenuActivity.class with the name of activity corresponding to Map

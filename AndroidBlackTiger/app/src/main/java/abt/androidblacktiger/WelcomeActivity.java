1e54059d63df4cc212a965e8642fb90e9c69de30
Maria47127 <maria.treacy@ucdconnect.ie>
2015-11-26 15:29:34 +0000

    #23 #24 resolved issues backbutton welcome screen and exiting

--- AndroidBlackTiger/app/src/main/java/abt/androidblacktiger/WelcomeActivity.java
+++ AndroidBlackTiger/app/src/main/java/abt/androidblacktiger/WelcomeActivity.java
@@ -95,6 +95,14 @@ public class WelcomeActivity extends Activity implements AdapterView.OnItemSelec
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
 }
 
 
